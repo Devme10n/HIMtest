1fd55fe3d3f39702c7bd14a16908d4c506525f83
kenway <kenway@naver.com>
2021-12-01 08:54:42 +0900

    Db 노랑딱지 제거

--- src/main/java/goodsDb/Db.java
+++ src/main/java/goodsDb/Db.java
@@ -6,7 +6,7 @@ import org.json.simple.parser.ParseException;
 
 //싱글톤패턴을 적용해서 db인스턴스가 한개만 있을 수 있도록 함.
 public class Db {
-    private static Db dbInstance = new Db();
+    private static final Db DB_INSTANCE = new Db();
     private static JSONObject allGoodsDb; //물품의 모든 정보가 저장되어있는 allGoodsDb. 클래스 내부에서만 접근 가능
 
     //생성자에서 allGoodsDb를 인스턴스가 생성될 때 1번만 초기 형태를 만들어줌
@@ -21,7 +21,7 @@ public class Db {
     }
     //Db클래스의 인스턴스를 전달해주는 메소드
     public static Db getInstance(){
-        return dbInstance;
+        return DB_INSTANCE;
     }
     //Db클래스의 allGoodsDb를 전달해주는 메소드
     public static JSONObject getdb(){
